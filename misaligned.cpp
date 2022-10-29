bd073b9888bfb2ae298399192cde5592d507eb57
Anupriya Manuja <Anupriya.Manuja@in.bosch.com>
2022-10-29 18:33:26 +0530

    fail the tests without fixing implementation

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <assert.h>
 
-int printColorMap() {
+std::string findMajorColor(int x) {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    return majorColor[x];
+}
+
+std::string findMinorColor(int x) {
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    return minorColor[x];
+}
+
+int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            std::cout << i * 5 + j << " | " << findMajorColor(i) << " | " << findMinorColor(i) << "\n";
         }
     }
     return i * j;
@@ -16,6 +24,8 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    int i = 2, j = 1;
+    assert( i*5+j +"|"+ findMajorColor(2) + "|" + findMinorColor(1) == "11 | Black | Orange");
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
